2e91062260a733af68110741376491c1ba23eb24
taylor <taylor@affair.dev>
2020-06-18 21:27:06 -0400

    added some logging in the application.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,7 +2,7 @@ import {
   Body,
   Controller,
   Delete,
-  Get,
+  Get, Logger,
   Param,
   ParseIntPipe,
   Patch,
@@ -24,13 +24,15 @@ import { GetUser } from '../auth/get-user.decorator';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
   constructor(private tasksService: TasksService) {}
 
   @Get()
   getAllTasks(
     @Query(ValidationPipe) filterDto: GetTasksFilterDto,
     @GetUser() user: User
-  ) {
+  ): Promise<Task[]> {
+    this.logger.verbose(`User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(filterDto)}`);
     return this.tasksService.getTask(filterDto, user);
   }
 
@@ -53,6 +55,7 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User
   ): Promise<Task> {
+    this.logger.verbose(`User "${user.username} creating a new task. Data ${JSON.stringify(createTaskDto)}"`);
     return this.tasksService.createTask(createTaskDto, user);
   }
 
