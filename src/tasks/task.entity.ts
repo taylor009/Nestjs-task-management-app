3d1f4432919a2c178919103eaa5259235dba3678
taylor <taylor@affair.dev>
2020-06-18 20:01:33 -0400

    created relationships for entities

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
+import { User } from '../auth/user.entity';
 
 
 @Entity()
@@ -15,4 +16,7 @@ export class Task extends BaseEntity {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne(type => User, user => user.task, { eager: false })
+  user: User;
 }
